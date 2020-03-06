01ebf4fe40da1d86281a0d937e2d96c9ff9beca9
Daniil Blagiy <blagiy.daniil@gmail.com>
2020-03-06 11:19:07 +0300

    working on command line work process chain

--- src/pomodoro.go
+++ src/pomodoro.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
-	//"bufio"
+	"bufio"
 	"runtime"
 	"os/exec"
-	"time"
+	"os/signal"
+	//"time"
+)
+
+type Mode int
+
+const (
+	Console Mode = 0
+	TimerRunning = 1
 )
 
 func Clear() {
@@ -22,12 +30,40 @@ func Clear() {
 }
 
 func main() {
-	for {
-		fmt.Println("A")
-		time.Sleep(1 * time.Second)
-		Clear()
-		fmt.Println("B")
-		time.Sleep(1 * time.Second)
-		Clear()
+	// Set up channel on which to send signal notifications.
+	// We must use a buffered channel or risk missing the signal
+	// if we're not ready to receive when the signal is sent.
+	signal_channel := make(chan os.Signal, 1)
+	signal.Notify(signal_channel, os.Interrupt)
+	s := <-signal_channel
+	fmt.Println("Got signal:", s)
+	
+	running := true
+	scanner := bufio.NewScanner(os.Stdin)
+	mode := Console
+
+	for running {
+		switch mode {
+		case Console:
+			if scanner.Scan() {
+				if scanner.Text() == "quit" || scanner.Text() == "exit" {
+					running = false
+				} else if scanner.Text() == "new" {
+					mode = TimerRunning
+				} else {
+					fmt.Println("Unresolved command")
+				}
+			}
+		case TimerRunning:
+			Clear()
+			fmt.Println("time display")
+			if scanner.Scan() {
+				if scanner.Text() == "stop" {
+					mode = Console
+				}
+			}
+		}
 	}
+
+	fmt.Println("Exiting Pomodoro")
 }
